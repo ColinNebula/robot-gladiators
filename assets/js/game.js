4040a89c42280e66fa3ee9bbe2206073e7b59096
warNebula <colinnebula@hotmail.com>
2021-07-10 15:41:40 -0400

    updated the js.script

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,22 +6,23 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-console.log(enemyNames. length);
-console.log(enemyNames[0]);
-console.log(enemyNames[1]);
-console.log(enemyNames[2]);
-for(var i = 0; i < enemyNames.length; i++) {
-  console.log(enemyNames[i]);
-  console.log(i);
-  console.log(enemyNames[i] + " is at " + i + " index");
-}
-
-var enemy1 = "Roborto";
-var enemy2 = "Amy Android";
-var enemy3 = "Robo Rumble";
+var enemyNames = [" Roborto", " Amy Android", " Robo Trumble "];
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+//console.log(enemyNames. length);
+//console.log(enemyNames[0]);
+//console.log(enemyNames[1]);
+//console.log(enemyNames[2]);
+//for(var i = 0; i < enemyNames.length; i++) {
+  //console.log(enemyNames[i]);
+  //console.log(i);
+ // console.log(enemyNames[i] + " is at " + i + " index");
+//}
+
+var enemy1 = " Roborto ";
+var enemy2 = " Amy Android ";
+var enemy3 = " Robo Rumble ";
 
 var fight = function() {
   // Alert players that they are starting the round
@@ -34,20 +35,20 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   // remove enemy's health by subtracting the amount set in the playerAttack variable
   enemyHealth = enemyHealth - playerAttack;
   console.log(
-    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
   );
 
   // check enemy's health
   if (enemyHealth <= 0) {
     window.alert(enemyName + " has died!");
   } else {
-    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    window.alert(enemyNames + " still has " + enemyHealth + " health left.");
   }
 
   // remove player's health by subtracting the amount set in the enemyAttack variable
   playerHealth = playerHealth - enemyAttack;
   console.log(
-    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
   );
 
   // check player's health
@@ -66,6 +67,7 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     window.alert(playerName + " has decided to skip this fight. Goodbye!");
     // subtract money from playerMoney for skipping
     playerMoney = playerMoney - 2;
+    console.log(playerMoney);
   }
   // if no (false), ask question again by running fight() again
   else {
@@ -74,9 +76,12 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 }
 }
 
-var fight = function(enemyName) {
+for (var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
+
   
-};
+
 // Game States
 // "WIN" - Player robot has defeated all enemy-robots
 //    * Fight all enemy-robots
